6fba7cc2a2af534e4cc5365e986725dfa410279e
Loker <therealloker@gmail.com>
2019-05-28 22:34:27 +0300

    add error representer and serializer

---
+++ app/concepts/lib/representer/error_representer.rb
@@ -0,0 +1,11 @@
+module Lib::Representer
+  class ErrorRepresenter < JSONAPI::Serializable::Error
+    detail do
+      @message
+    end
+
+    source do
+      pointer "/data/attributes/#{@field.to_s.dasherize}" unless @field.nil?
+    end
+  end
+end
