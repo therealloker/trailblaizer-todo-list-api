b27708d821519126eee56fb327b96b40a577c01e
Loker <therealloker@gmail.com>
2019-06-18 00:07:34 +0300

    add test for doc file selector service

--- spec/requests/v1/users/registration_spec.rb
+++ spec/requests/v1/users/registration_spec.rb
@@ -1,5 +1,3 @@
-require 'rails_helper'
-
 RSpec.describe 'V1::Users::Registrations API', type: :request do
   include Docs::V1::Users::Registrations::Api
 
---
+++ spec/services/doc_selector_service_spec.rb
@@ -0,0 +1,13 @@
+RSpec.describe Api::DocSelectorService do
+  describe '.call' do
+    let(:version) { FFaker::Internet.domain_word }
+
+    it 'returns pathname instance' do
+      expect(described_class.call(version)).to be_an_instance_of(Pathname)
+    end
+
+    it do
+      expect(described_class.call(version).to_s).to match(/docs\/#{version}.html/)
+    end
+  end
+end
