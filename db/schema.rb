7093c0e72345c4f9ff146246be89dca4bf722165
Loker <therealloker@gmail.com>
2019-06-25 21:34:39 +0300

    add projects and tasks models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_26_153253) do
+ActiveRecord::Schema.define(version: 2019_06_25_183212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.bigint "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_tasks_on_project_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -22,4 +38,6 @@ ActiveRecord::Schema.define(version: 2019_05_26_153253) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "projects", "users"
+  add_foreign_key "tasks", "projects"
 end
