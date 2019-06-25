b632f18820d3309fcd72fb92287c1386519511c6
Loker <therealloker@gmail.com>
2019-06-25 22:40:43 +0300

    add controllers

---
+++ app/controllers/v1/tasks_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module V1
+  class TasksController < ApplicationController
+    before_action :authorize_request!
+
+    def index
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+  end
+end
