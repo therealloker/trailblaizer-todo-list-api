0eceae3c94fd8e01d52c4d10990aafb6e1ebf8cb
Loker <therealloker@gmail.com>
2019-07-06 02:35:47 +0300

    test delete task reqest

--- app/concepts/v1/tasks/operation/destroy.rb
+++ app/concepts/v1/tasks/operation/destroy.rb
@@ -3,7 +3,7 @@
 module V1::Tasks::Operation
   class Destroy < Trailblazer::Operation
     step Policy::Guard(V1::Tasks::Lib::Policy::TaskAccessGuard.new), fail_fast: true
-    step Model(Project, :find)
+    step Model(Task, :find)
     step :destroy
 
     def destroy(_ctx, model:, **)
