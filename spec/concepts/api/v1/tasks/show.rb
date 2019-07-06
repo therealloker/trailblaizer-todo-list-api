5da99633d8fac219bc209370a7aed2fbeb33a80c
Loker <therealloker@gmail.com>
2019-07-06 20:30:30 +0300

    show task operation tests

---
+++ spec/concepts/api/v1/tasks/show.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+RSpec.describe V1::Tasks::Operation::Show do
+  subject(:result) { described_class.call(params: params, current_user: user) }
+
+  let(:user) { create(:user) }
+  let(:valid_task) { create(:task, user: user) }
+  let(:params) do
+    {
+      id: task.id
+    }
+  end
+
+  describe 'Success' do
+    context 'when the task relate to the user' do
+      let(:task) { valid_task }
+
+      it 'shows the task' do
+        expect(result['result.policy.default']).to be_success
+        expect(result[:errors]).to be_nil
+        expect(result[:model]).to eq task
+        expect(result).to be_success
+      end
+    end
+  end
+
+  describe 'Fail' do
+    context 'when the task does not relate to the user' do
+      let(:task) { create(:task) }
+
+      it 'does not show the task' do
+        expect(result['result.policy.default']).to be_failure
+        expect(result).to be_failure
+      end
+    end
+  end
+end
