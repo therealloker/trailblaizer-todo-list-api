3e4cedc76bec983e73ea73ad1abb04f1227381b3
Loker <therealloker@gmail.com>
2019-07-11 23:23:45 +0300

    index operation test

--- spec/concepts/api/v1/tasks/index_spec.rb
+++ spec/concepts/api/v1/tasks/index_spec.rb
@@ -4,36 +4,62 @@ RSpec.describe V1::Tasks::Operation::Index do
   subject(:result) { described_class.call(current_user: user, params: params) }
 
   let(:user) { create(:user) }
-  let!(:other_users_task) { create(:task) }
 
   describe 'Success' do
+    let!(:task) { create(:task) }
+
     context 'without any filters' do
-      let!(:user_tasks) { create_list(:task, 2, user: user) }
       let(:params) { {} }
+      let!(:user_tasks) { create_list(:task, 2, user: user) }
 
       it 'returns all the users tasks' do
         expect(result[:errors]).to be_nil
         expect(result[:model]).to eq user_tasks
-        expect(result[:model]).not_to include(other_users_task)
+        expect(result[:model]).not_to include(task)
         expect(result).to be_success
       end
     end
 
     context 'with filter by done' do
+      let(:params) { { done: true } }
+      let!(:user_task) { create(:task, user: user) }
+      let!(:done_user_tasks) { create_list(:task, 2, user: user, done: true) }
+
       it 'returns only users tasks with done status' do
         expect(result[:errors]).to be_nil
-        expect(result[:model]).to eq user_tasks
-        expect(result[:model]).not_to include(other_users_task)
+        expect(result[:model]).to eq done_user_tasks
+        expect(result[:model]).not_to include(task)
+        expect(result[:model]).not_to include(user_task)
         expect(result).to be_success
       end
     end
 
     context 'with filter by important' do
-      it 'returns only users tasks with status' do
+      let(:params) { { important: true } }
+      let!(:user_task) { create(:task, user: user) }
+      let!(:important_user_tasks) { create_list(:task, 2, user: user, important: true) }
+
+      it 'returns only important users tasks' do
+        expect(result[:errors]).to be_nil
+        expect(result[:model]).to eq important_user_tasks
+        expect(result[:model]).not_to include(task)
+        expect(result[:model]).not_to include(user_task)
+        expect(result).to be_success
       end
     end
   end
 
   describe 'Fail' do
+    context 'when filter params has invalid types' do
+      let(:params) { { important: 'who knows', done: 2 } }
+      let(:errors) do
+        {
+          important: ['must be boolean'],
+          done: ['must be boolean']
+        }
+      end
+
+      include_examples 'has validation errors'
+    end
   end
 end
