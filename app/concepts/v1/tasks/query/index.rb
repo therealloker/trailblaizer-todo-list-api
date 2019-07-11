6875607d3c27569673e7ccb630eaf94ed96facea
Sergey Kozhemiako <therealloker@gmail.com>
2019-07-11 19:31:08 +0300

    add filter to task

--- app/concepts/v1/tasks/query/index.rb
+++ app/concepts/v1/tasks/query/index.rb
@@ -2,8 +2,12 @@
 
 module V1::Tasks::Query
   class Index
-    def self.call(user_id, relation = Task.all)
-      relation.where(user_id: user_id).order(:created_at)
+    def self.call(user_id:, important: nil, done: nil, relation: Task.all)
+      scoped = relation.where(user_id: user_id)
+      scoped = scoped.where(important: important) unless important.nil?
+      scoped = scoped.where(done: done) unless done.nil?
+      scoped = scoped.order(:created_at)
+      scoped
     end
   end
 end
