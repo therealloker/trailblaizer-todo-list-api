25f0bd1edff7892d33310f0366b475f176b3456c
Loker <therealloker@gmail.com>
2019-07-12 00:26:16 +0300

    add some broken tests ))

---
+++ spec/concepts/api/v1/tasks/update_spec.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+RSpec.describe V1::Tasks::Operation::Update do
+  subject(:result) { described_class.call(params: params, current_user: user) }
+
+  let(:user) { create(:user) }
+  let(:task) { create(:task, name: 'get rest', user: user) }
+  let(:valid_params) do
+    {
+      id: task.id,
+      name: 'make test',
+      important: 'true',
+      done: '1'
+    }
+  end
+
+  describe 'Success' do
+    let(:params) { valid_params }
+
+    it 'updates the task' do
+      expect { result; task.reload }.to change(task, :name).from('get rest').to('make test')
+      expect { result; task.reload }.to change(task, :done).from(false).to(true)
+      expect { result; task.reload }.to change(task, :important).from(false).to(true)
+      expect(result[:errors]).to be_nil
+      expect(result[:model]).to be_persisted
+      expect(result).to be_success
+    end
+  end
+
+  describe 'Fail' do
+    context 'when name is empty' do
+      let(:params) { { id: task.id } }
+      let(:errors) do
+        {
+          name: ['must be filled']
+        }
+      end
+
+      include_examples 'has validation errors'
+    end
+
+    context 'when name is not unique' do
+      let(:errors) { { name: ['You already have a task with such name'] } }
+      let(:params) { valid_params }
+
+      before { create(:task, name: params[:name], user: user) }
+
+      include_examples 'has validation errors'
+    end
+
+    context 'when keys has incorrect types' do
+      let(:errors) { { important: ['must be boolean'], done: ['must be boolean'] } }
+      let(:params) do
+        {
+          id: task.id,
+          name: valid_params[:name],
+          important: 'who knows',
+          done: 'no('
+        }
+      end
+
+      include_examples 'has validation errors'
+    end
+  end
+end
