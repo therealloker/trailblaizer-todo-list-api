03e9e0b9d8e2a9415e03787fad7faa0171998502
Loker <therealloker@gmail.com>
2019-07-22 16:56:07 +0300

    update secret key for heroku

--- app/concepts/v1/lib/auth/json_web_token.rb
+++ app/concepts/v1/lib/auth/json_web_token.rb
@@ -2,7 +2,7 @@
 
 module V1::Lib::Auth
   class JsonWebToken
-    SECRET_KEY = Rails.application.secrets.secret_key_base
+    SECRET_KEY = Rails.application.secret_key_base
 
     def self.encode(payload, exp = 36.hours.from_now)
       payload[:exp] = exp.to_i
