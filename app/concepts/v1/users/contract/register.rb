f69a5b63d9776a104280da605c893fe1eca61faf
Loker <therealloker@gmail.com>
2019-07-22 18:10:28 +0300

    change password confirmation validation logic

--- app/concepts/v1/users/contract/register.rb
+++ app/concepts/v1/users/contract/register.rb
@@ -6,7 +6,7 @@ module V1::Users::Contract
 
     property :email
     property :password
-    property :password_confirmation
+    property :password_confirmation, virtual: true
 
     validation :default do
       required(:email).filled(format?: App.config.user.magic_email_regex)
@@ -14,10 +14,23 @@ module V1::Users::Contract
         :str?,
         min_size?: App.config.user.password_min_length,
         format?: App.config.user.magic_password_regex
-      ).confirmation
+      )
     end
 
-    validation :email_unique, if: :default do
+    validation :same_password, with: { form: true }, if: :default do
+      configure do
+        option :form
+        config.messages = :i18n
+
+        def same_password?(value)
+          value == form.password
+        end
+      end
+
+      required(:password_confirmation).filled(:same_password?)
+    end
+
+    validation :email_unique, if: :same_password do
       configure do
         config.messages = :i18n
 
